ac19617677e9a70870b0ea9015dbd2bd78683e24
Nir Moav <getnirm@gmail.com>
2015-01-17 20:49:26 +0200

    creating an initial search bar react component.

---
+++ StockSearchBar.js
@@ -0,0 +1,8 @@
+define(['lodash', 'react'], function(_, React) {
+	return React.createClass({
+		displayName: "StockSearchBar",
+		render: function() {
+			return React.createElement("div", {}, "Search Bar");
+		}
+	});
+});
--- stocks.js
+++ stocks.js
@@ -1,10 +1,10 @@
-define(['react', 'stockTable'], function(React, StockTable) {
+define(['react', 'stockTable', 'StockSearchBar'], function(React, StockTable, StockSearchBar) {
 	'use strict';
 
 	return React.createClass({
 		displayName: "Stocks",
 		render: function() {
-			var children = ["Search Bar", React.createElement(StockTable, {})];
+			var children = [React.createElement(StockSearchBar, {}), React.createElement(StockTable, {})];
 			return React.createElement('div', {style: {width: '300px'}}, children);
 		}
 	});

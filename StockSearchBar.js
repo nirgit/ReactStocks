07c73e6fb6c3bbe3b6ceeac0abcea2b59ba1758f
Nir Moav <getnirm@gmail.com>
2015-01-17 21:03:46 +0200

    adding input for stock search bar

--- StockSearchBar.js
+++ StockSearchBar.js
@@ -1,8 +1,18 @@
 define(['lodash', 'react'], function(_, React) {
 	return React.createClass({
 		displayName: "StockSearchBar",
+		getInitialState: function() {
+			return {'searchText': ''};
+		},
 		render: function() {
-			return React.createElement("div", {}, "Search Bar");
+			var searchField = React.createElement("input", 
+				{
+					"type": "text", 
+					"onChange": function(evt) {
+						this.setState({'searchText': evt.target.value});
+					}
+			}, this.state.searchText);
+			return React.createElement("div", {}, ["Search stock: ", searchField]);
 		}
 	});
 });
